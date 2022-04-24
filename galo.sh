1e7ed577e64b192bf7a16e4c442966bb6d69e326
John D. Martin III <john.d.martin.iii@gmail.com>
2022-04-24 14:48:51 -0400

    Revert "parse option logic"
    This reverts commit 1ba6da8b4c35a5f17453ade660418ac2854fdc16.

--- galo.sh
+++ galo.sh
@@ -15,17 +15,29 @@ if [ -z "$DEPCURL" ] || [ -z "$DEPJQ" ]; then
 fi
 # Show help function
 function show_help () {
-	printf -- "Usage: $0 [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n"
-	printf -- "\n"
-  printf -- "  -h\t\tShow this help message and exit.\n"
-  printf -- "  -n, -s\tLatitude: N positive; S negative.\n"
-  printf -- "  -e, -w\tLongitude: E positive; W negative.\n"
-  printf -- "  -z\t\tTime zone: uses /etc/timezone by default.\n"
-  printf -- "  -d 0-6\tDay to retrieve weather: 0 is today; defaults to 1.\n"
-  printf -- "  -v\t\tVerbose output: returns full weather forecast.\n"
-  printf -- "  -j\t\tEcho raw JSON from open-meteo API.\n"
-#  return 0	
+	printf "Usage: $0 [options] -l LATITUDE LONGITUDE -z TIME_ZONE\n"
+	printf "\n"
+  printf "-h  Show this help message and exit.\n"       
+  printf "-l  Latitude and longitude of location (stores or updates config variable in /home/$USER/.galosh)\n"
+  printf "-z  Time zone (uses /etc/timezone by default)\n"
+  printf "-n  Get the weather now instead of tomorrow\n"
+  printf "-v  Verbose output (full weather forecast for the week)\n"
+  printf "-d  Echo raw JSON from open-meteo API\n"
+	return 0	
 }
+# Parse help option
+while getopts ":h" opt; do
+  case ${opt} in
+    h )
+      show_help
+      exit 0
+      ;;
+    \?)
+      printf "Invalid option: -$OPTARG\n" 1>&2
+      exit 1
+      ;;
+  esac
+done    
 
 #if []; then
 #
@@ -41,86 +53,25 @@ LONG=-79.0469
 TZ=$(cat /etc/timezone)
 
 
-# Get one week of weather data from open-meteo.com
-function getdata () {
+# Get one week of rain data from open-meteo.com
+function getdata_simple(){
   curl -s -G \
     -d "latitude=${LAT}" \
     -d "longitude=${LONG}" \
-    -d "daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant" \
-    -d "current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch" \
+    -d "daily=precipitation_sum,precipitation_hours" \
+    -d "temperature_unit=fahrenheit" \
+    -d "windspeed_unit=mph" \
+    -d "precipitation_unit=inch" \
     -d "timezone=${TZ}" \
     https://api.open-meteo.com/v1/forecast
 }
-# Output pretty JSON
-function show_json () {
-  echo ${DATA} | jq
-}
 
-dump () {
-  echo "ERROR: $*." >&2
-  exit 1
-}
+DATA=$(getdata_simple)
 
-# Store JSON obeject from open-meteo
-DATA=$(getdata)
+TOMORROW_PRECIP=$(jq -c '.daily.precipitation_sum[1]' <<< ${DATA})
 
-#echo "${LAT} ${LONG}" | tr -d '-'
-#tr -d '-' <<< "${LAT} ${LONG}"
-
-OPT_N=false
-OPT_S=false
-OPT_E=false
-OPT_W=false
-LAT_IN_COUNT=0
-LONG_IN_COUNT=0
-
-# Parse run-then-exit options
-while getopts ":hjz:n:s:e:w:" opt; do
-  case ${opt} in
-    z )
-      TZ_IN=$OPTARG
-      ;;
-    n )
-      $OPT_S || [ ! $OPT_W -a ! $OPT_E ] && dump "Must specify LATITUDE and LONGITUDE"
-      OPT_N=true
-      LAT_IN=$(tr -d '-' <<< $OPTARG)
-      ;;
-    s )
-      $OPT_N || [ ! $OPT_W -a ! $OPT_E ] && dump "Must specify LATITUDE and LONGITUDE"
-      OPT_S=true
-      LAT_IN=-$(tr -d '-' <<< $OPTARG)
-      ;;
-    e )
-      $OPT_W || [ ! $OPT_N -a ! $OPT_S ] && dump "Must specify LATITUDE and LONGITUDE"
-      OPT_E=true
-      LONG_IN=$(tr -d '-' <<< $OPTARG)
-      ;;
-    w )
-      $OPT_E || [ ! $OPT_N -a ! $OPT_S ] && dump "Must specify LATITUDE and LONGITUDE"
-      OPT_W=true
-      LONG_IN=-$(tr -d '-' <<< $OPTARG)
-      ;;
-    h )
-      show_help
-      exit 0
-      ;;
-    j )
-      show_json
-      exit 0
-      ;;
-    \? )
-      dump "Invalid option: -$OPTARG\n"
-      exit 1
-      ;;
-  esac
-done
-
-echo "${LAT_IN} ${LONG_IN}"
-
-#TARGET_PRECIP=$(jq -c ".daily.precipitation_sum["$TARGET_INDEX]" <<< ${DATA})
-
-#if [[ ${TARGET_PRECIP} > 0 ]]; then
-#  printf "You might need your galoshes tomorrow.\n"
-#else
-#  printf "You probably won't need your galoshes tomorrow.\n"
-#fi
+if [[ ${TOMORROW_PRECIP} > 0 ]]; then
+  printf "You might need your galoshes tomorrow.\n"
+else
+  printf "You probably won't need your galoshes tomorrow.\n"
+fi
